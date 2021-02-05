1e0a9cd4f0f94510d3c64a7af0247882e040c126
Celeste Graham <clgraham74@gmail.com>
2021-02-05 18:51:03 +0000

    changed string to Hello, world!

--- hello.js
+++ hello.js
@@ -2,6 +2,6 @@ function hello() {
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
 	return "Hello, world!";
-  }
+}
  
 module.exports = hello;
